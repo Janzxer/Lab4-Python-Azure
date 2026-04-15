71d93678d291463859fceda6b8b9cd5960395d8f
Janne <janne.viljanen@hotmail.com>
2026-04-15 14:28:28 +0300

    workflow testi

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Python pyörii Sinisessä Amazonissa!"
+    return "Python pyörii Sinisessä Amazonissa! | 15.4.2026 integraation testaus"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
